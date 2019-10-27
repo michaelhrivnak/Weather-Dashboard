0df0e198455c0e379e0bdc18e1f8154f24ac35d0
Michael Hrivnak <mhrivnak@gmail.com>
2019-10-26 23:49:31 -0400

    finsihed logic for current weather display

--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,10 @@ const searchInput = $("#searchInput");
 const searchButton = $("#searchBtn");
 const currentWeatherDiv = $("#currentWeather");
 const forecastDiv = $("#forecast");
+const clearBtn = $("#clear");
 var storedSearches = getStoredSearches();
 var addedCity = newCity();
-var unts = {deg:"C",}
+var units = {deg:"C",speed:"kph"};
 // Here we are building the URL we need to query the database
 //var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+CityQuery+"&units=metric&appid="+APIKey;
 //var iconUrl = "https://openweathermap.org/img/wn/"+icon+"@2x.png"
@@ -21,7 +22,7 @@ function init(){
         }
     });
     searchButton.on("click", searchButtonClicked );
-    testFunction(performAPICall, "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&units=metric&appid=" + APIKey);    
+    clearBtn.on("click",function(){clearLocalStorage(lsKey)});    
 }
 
 function buildSearchHistory(){
@@ -31,7 +32,7 @@ function buildSearchHistory(){
             searchesDiv.append(
                 $("<button>")
                     .text(correctCase(element.city) +", "+element.country.toUpperCase())
-                    .on("click", function (){
+                    .on("click", function (){                        
                         getWeather(element);
                     })
             );
@@ -41,70 +42,116 @@ function buildSearchHistory(){
 
 function searchButtonClicked(){
     let cityVal = searchInput.val().trim();
-    let city = newCity(cityVal, null);
-    addedCity = city;
-    getWeather(city);        
+    let city = newCity(cityVal, null);       
+    getWeather(city);
+    searchInput.val("");        
 }
 
 
 function getWeather(city){
-    let queryUrlCurrent = "";
-    let queryUrlForcast = "";
+    addedCity = city; 
+    let queryURLCurrent = "";
+    let queryURLForcast = "";
 
     if(city.country == null){
         queryURLCurrent = "https://api.openweathermap.org/data/2.5/weather?q="+city.city+"&units=metric&appid="+APIKey;
         queryURLForcast = "";
     }else{
+        
         queryURLCurrent = "https://api.openweathermap.org/data/2.5/weather?q="+city.city+","+city.country+"&units=metric&appid="+APIKey;
         queryURLForcast = "";
     }
     
-    performAPICall(queryUrlCurrent, getCurrentWeather);
-    performAPICall(queryUrlForecast, getWeatherForcast);    
+    performAPIGETCall(queryURLCurrent, getCurrentWeather);
+    //performAPIGETCall(queryURLForecast, getWeatherForcast);    
 }
 
 function getCurrentWeather(data){
-    currentWeatherDiv.empty();
-    currentWeatherDiv.append(
-                        $("<h2>").text(correctCase(data.name)+", "
-                        +data.country.toUpperCase()
-                        +data.dt)
-                        ,$("<img>").attr("src", "https://openweathermap.org/img/wn/"+data.weather.icon+"@2x.png" )
-                        ,$("<p>").text("Temperature: " + data.main.temp + "°"+deg)
+    //console.log(data);
+    if(data != null){
+        currentWeatherDiv.empty();
+        currentWeatherDiv.append(
+                            $("<h2>").text(correctCase(data.name)+", "
+                                    +data.sys.country.toUpperCase()+" "
+                                    +moment.unix(data.dt).format("dddd, MMM Do YYYY"))
+                                    .append($("<img>").attr("src", "https://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png"))
+                            ,$("<p>").text("Temperature: " + data.main.temp + "°"+units.deg)
+                            ,$("<p>").text("Humidity: "+ data.main.humidity+"%")
+                            ,$("<p>").text("Wind Speed: "+ data.wind.speed+" "+units.speed)
+                            ,$("<p>").text("UV Index: ").append($("<div>").attr("id", "UVIndex"))
+        );
 
-    );
+        let UVqueryURL = "https://api.openweathermap.org/data/2.5/uvi?appid="+APIKey+"&lat="+data.coord.lat+"&lon="+data.coord.lon;
+        
+        performAPIGETCall(UVqueryURL,getUV);
 
-    $(".city").text(response.name);
-    $('.wind').text(response.wind.speed+"mph");
-    $('.humidity').text(response.main.humidity+"%");
-    $('.temp').text(response.main.temp+" degrees(c)");
-    
-    if(addedCity.country == null){
-        addedCity.country = data.country;
-        addedCity.city = data.name;
-    }
-    addNewCity(addedCity);
-    addedCity = null;            
+        if(addedCity.country == null){
+            addedCity.country = data.sys.country;
+            addedCity.city = data.name;
+            addNewSearch(addedCity);
+            addedCity = null;
+        }
+        
+    }else{
+        alert("Something went wrong getting current weather data, please try again");
+    }            
 }
 
 function getWeatherForecast(data){
+    if(data != null){
 
+    }else{
+        alert("Something went wrong getting forecast data, please try again");
+    }
 }
 
+function getUV(data){
+    if(data != null){
+        let UVIndex = data.value;
+        let UVDiv = $("#UVIndex");
+        let UVbg = null;
+        let textColor = null;
+        if(UVIndex < 2){
+            UVbg = "green";
+            textColor = "white";
+        }else if (UVIndex < 6){
+            UVbg = "yellow";
+        }else if (UVIndex < 8){
+            UVbg = "orange";
+        }else if (UVIndex < 11){
+            UVbg = "red";
+            textColor = "white";
+        }else{
+            UVbg = "violet";
+        }
+        UVDiv.css("background-color",UVbg);
+        if(textColor != null){
+            UVDiv.css("color",textColor);
+        }
+        UVDiv.text(UVIndex);
+    }else{
+        alert("Something went wrong getting UV data, please try again");
+    }
+}
 
-
-function performAPICall(queryURL, callbackFunction){
-    $.ajax({url: queryURL, method: "GET"}).then(callbackFunction(response));   
+function performAPIGETCall(queryURL, callbackFunction){    
+    $.ajax({url: queryURL, method: "GET"}).then(function(response){
+        callbackFunction(response);
+    });   
 }
 
 function addNewSearch(city){
+    //console.log(city, storedSearches);
+    if(storedSearches == null){
+        storedSearches = [];
+    }
     storedSearches.unshift(city);
     saveSearches();
     buildSearchHistory();
 }
 
-function saveSearches(searchArr){
-    localStorage.setItem(lsKey,searchArr)
+function saveSearches(){
+    localStorage.setItem(lsKey,JSON.stringify(storedSearches));
 }
 
 init();
@@ -115,11 +162,15 @@ function getDayOfWeek(date){
 }
 
 function correctCase(str){
-    return str.toLowerCase().replace(/\b\w/, l >= l.toUpperCase());
+    return str.toLowerCase().replace(/\b\w/g, l => l.toUpperCase());
 }
 
 function getStoredSearches(){
-    return localStorage.getItem(lsKey);
+    return JSON.parse(localStorage.getItem(lsKey));
+}
+
+function clearLocalStorage(key){
+    localStorage.removeItem(key);
 }
 
 function newCity(city, country){
